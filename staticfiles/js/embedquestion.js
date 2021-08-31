3be37b8fa14ab72762a58ae6ee3463bdaa41a214
johntygomes <jhonty.gomes@trialcard.com>
2021-08-31 21:10:41 +0530

    display username

--- staticfiles/js/embedquestion.js
+++
@@ -1,6 +0,0 @@
-fetch(rootUrl + "api/get-question-data/")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        document.querySelector("#embed-question-body").innerHTML = data.body;
-    })
