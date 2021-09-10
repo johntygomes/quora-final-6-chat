f97584f609381f15778fa01f157f6cafab0c2b65
johntygomes <jhonty.gomes@trialcard.com>
2021-09-10 20:08:36 +0530

    chat ui added

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -99,8 +99,8 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-'''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
